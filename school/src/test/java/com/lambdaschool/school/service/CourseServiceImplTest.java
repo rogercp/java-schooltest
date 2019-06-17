6fa2805f3eae20fcbf83bb88f306e9d417673d6b
Roger Caballero <rogercp1234@yahoo.com>
2019-06-17 14:09:54 -0700

    delete and deleteNotFound tests passing

--- school/src/test/java/com/lambdaschool/school/service/CourseServiceImplTest.java
+++ school/src/test/java/com/lambdaschool/school/service/CourseServiceImplTest.java
@@ -1,20 +1,26 @@
 package com.lambdaschool.school.service;
 
 import com.lambdaschool.school.SchoolApplication;
+import com.lambdaschool.school.SchoolApplication;
+import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Instructor;
+import com.lambdaschool.school.model.Student;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.*;
- import static junit.framework.TestCase.assertEquals;
- import static junit.framework.TestCase.assertNotNull;
  
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SchoolApplication.class)
@@ -51,7 +57,18 @@ public class CourseServiceImplTest
     @Test
     public void delete()
     {
+        courseService.delete(6);
+        assertEquals(5,courseService.findAll().size());
     }
+
+    @Test(expected= EntityNotFoundException.class)
+    public void deleteNotFound()
+    {
+        courseService.delete(23324234);
+        assertEquals(2,courseService.findAll().size());
+    }
+
+    
 
     @Test
     public void findCourseById()
